0e36ec101eef8211b82ed372d673921b0e2cd297
BhavanaDodla <dbhavana7777@gmail.com>
2024-07-29 14:45:20 -0500

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args): }
+
+
+}
